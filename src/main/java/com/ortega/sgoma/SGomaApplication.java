d55b87deb9d0311ca237ec60303457989e7cf96f
ortega <kabweortega@gmail.com>
2024-02-02 16:20:34 +0200

    Feat: Add goma server controller

---
+++ src/main/java/com/ortega/sgoma/SGomaApplication.java
@@ -0,0 +1,13 @@
+package com.ortega.sgoma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SGomaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SGomaApplication.class, args);
+    }
+
+}
