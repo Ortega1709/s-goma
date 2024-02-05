55e1a4107012ddc2dcae12fb0d4e546dfb966a41
ortega <kabweortega@gmail.com>
2024-02-05 08:29:02 +0200

    Build: Add spring cloud routing dependency

--- src/main/java/com/ortega/sgoma/SGomaController.java
+++ src/main/java/com/ortega/sgoma/SGomaController.java
@@ -1,4 +1,4 @@
-package com.ortega.sgoma.controllers;
+package com.ortega.sgoma;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
