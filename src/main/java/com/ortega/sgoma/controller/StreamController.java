ddd7089faf8fe62c0b16fd240e5490e7ac56e9e0
ortega <kabweortega@gmail.com>
2024-02-14 09:42:37 +0200

    Feat: Add response link of optimal server

--- src/main/java/com/ortega/sgoma/controller/StreamController.java
+++ src/main/java/com/ortega/sgoma/controller/StreamController.java
@@ -2,18 +2,22 @@ package com.ortega.sgoma.controller;
 
 import com.ortega.sgoma.services.StreamService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Slf4j
+@RestController
 @AllArgsConstructor
-@RequestMapping("/api/v1/stream/songs")
+@RequestMapping("api/v1/")
 public class StreamController {
 
     private StreamService streamService;
-    @GetMapping(value = "/stream/songs", produces = "audio/mp3")
+    @GetMapping(value = "stream/songs", produces = "audio/mp3")
     public Mono<Resource> getSongByLink(@RequestParam("link") String link) {
         return streamService.getSong(link);
     }
